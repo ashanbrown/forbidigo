b6923372d558bcda3c3339f8e6f1dcade2941eca
Andrew S. Brown <andrew@launchdarkly.com>
2021-01-31 08:23:58 -0800

    Ignore godoc examples by default
    godoc examples (https://blog.golang.org/examples) depend on being able to write
    to stdout, which is in contradiction to the default forbidden pattern of fmt.Print*.
    To make this work nicely out of the box, we will skip checking godoc examples, by default.
    This includes "whole file examples", in which an entire file is ignored because
    it has only a single Example* method.
    
    This PR also allows the `//permit` directive to be ignored, allowing this
    linter to be controlled just using the standard `//nolint` directive from
    golangci lint.
    
    This PR also adds a dependency on the launchdarkly/go-options option code
    generator, which isn't pinned in the repo.  I may consider doing that at a
    later date.
    
    Fixes #3

--- forbidigo/forbidigo_test.go
+++ forbidigo/forbidigo_test.go
@@ -38,10 +38,86 @@ func foo() {
 	fmt.Printf("here i am") // permit:fmt.Printf
 }`)
 	})
+
+	t.Run("has option to ignore permit directives", func(t *testing.T) {
+		linter, _ := NewLinter([]string{`fmt\.Printf`}, OptionIgnorePermitDirectives(true))
+		issues := parseFile(t, linter, "file.go", `
+package bar
+
+func foo() {
+	fmt.Printf("here i am") // permit:fmt.Printf
+}`)
+		assert.NotEmpty(t, issues)
+	})
+
+	t.Run("examples are ignored by default in test files", func(t *testing.T) {
+		linter, _ := NewLinter([]string{`fmt\.Printf`})
+		issues := parseFile(t, linter, "file_test.go", `
+package bar
+
+func ExampleFoo() {
+	fmt.Printf("here i am")
+}`)
+		assert.Empty(t, issues)
+	})
+
+	t.Run("whole file examples are ignored by default", func(t *testing.T) {
+		linter, _ := NewLinter([]string{`fmt\.Printf`})
+		issues := parseFile(t, linter, "file_test.go", `
+package bar
+
+func Foo() {
+	fmt.Printf("here i am")
+}
+
+func Example() {
+	Foo()
+}`)
+		assert.Empty(t, issues)
+	})
+
+	t.Run("Test functions prevent a file from being considered a whole file example", func(t *testing.T) {
+		linter, _ := NewLinter([]string{`fmt\.Printf`})
+		issues := parseFile(t, linter, "file_test.go", `
+package bar
+
+func TestFoo() {
+	fmt.Printf("here i am")
+}
+
+func Example() {
+}`)
+		assert.NotEmpty(t, issues)
+	})
+
+	t.Run("Benchmark functions prevent a file from being considered a whole file example", func(t *testing.T) {
+		linter, _ := NewLinter([]string{`fmt\.Printf`})
+		issues := parseFile(t, linter, "file_test.go", `
+package bar
+
+func BenchmarkFoo() {
+	fmt.Printf("here i am")
+}
+
+func Example() {
+}`)
+		assert.NotEmpty(t, issues)
+	})
+
+	t.Run("examples can be included", func(t *testing.T) {
+		linter, _ := NewLinter([]string{`fmt\.Printf`}, OptionIgnoreGodocExamples(false))
+		issues := parseFile(t, linter, "file.go", `
+package bar
+
+func ExampleFoo() {
+	fmt.Printf("here i am")
+}`)
+		assert.NotEmpty(t, issues)
+	})
 }
 
 func expectIssues(t *testing.T, linter *Linter, contents string, issues ...string) {
-	actualIssues := parseFile(t, linter, contents)
+	actualIssues := parseFile(t, linter, "testing.go", contents)
 	actualIssueStrs := make([]string, 0, len(actualIssues))
 	for _, i := range actualIssues {
 		actualIssueStrs = append(actualIssueStrs, i.String())
@@ -49,9 +125,9 @@ func expectIssues(t *testing.T, linter *Linter, contents string, issues ...strin
 	assert.ElementsMatch(t, issues, actualIssueStrs)
 }
 
-func parseFile(t *testing.T, linter *Linter, contents string) []Issue {
+func parseFile(t *testing.T, linter *Linter, fileName, contents string) []Issue {
 	fset := token.NewFileSet()
-	expr, err := parser.ParseFile(fset, "testing.go", contents, parser.ParseComments)
+	expr, err := parser.ParseFile(fset, fileName, contents, parser.ParseComments)
 	if err != nil {
 		t.Fatalf("unable to parse file contents: %s", err)
 	}
