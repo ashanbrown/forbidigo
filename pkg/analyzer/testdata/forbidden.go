386d6bacb0c765741088d3dd8a340e3d1734e176
Patrick Ohly <patrick.ohly@intel.com>
2022-11-15 18:11:51 +0100

    default pattern: match against fmt package instead of name
    Previously, import renaming led to false negatives:
    
       import fmt2 "fmt"
    
       fmt2.Println("hello world")
    
    There were also false positives for source code that didn't
    use the fmt package and just looked like it did:
    
       fmt := struct {
              Println string
       }{}
       return fmt.Println
    
    Now the default pattern uses the new import renaming support and
    only matches when the actual "fmt" standard package provides the
    forbidden symbol.

--- pkg/analyzer/testdata/forbidden.go
+++ pkg/analyzer/testdata/forbidden.go
@@ -10,9 +10,9 @@ import (
 func Foo() {
 	fmt.Println("here I am") // want "forbidden by pattern"
 	fmt.Printf("this is ok") //permit:fmt.Printf // this is ok
-	print("not ok")          // want "forbidden by pattern"
-	println("also not ok")   // want "forbidden by pattern"
-	alias.Println("hello")   // not matched by default pattern fmt.Println
+	print("not ok")          // no package, not matched
+	println("also not ok")   // no package, not matched
+	alias.Println("hello")   // want "forbidden by pattern"
 	pkg.Forbidden()          // want "pkg.Forbidden.*forbidden by pattern .*example.com/some/pkg.*Forbidden"
 }
 
@@ -20,5 +20,5 @@ func Bar() string {
 	fmt := struct {
 		Println string
 	}{}
-	return fmt.Println // want "forbidden by pattern"
+	return fmt.Println // not the fmt package, not matched
 }
